c7cee93123b5fa4b7dc684f8752355ea2c58d979
srija-1189 <knagasrija01@gmail.com>
2026-06-25 10:59:55 +0530

    Revert map debugging changes

--- public/js/map.js
+++ public/js/map.js
@@ -2,32 +2,24 @@ console.log("map loaded");
 
 const mapDiv = document.getElementById("map");
 
-if (mapDiv) {
+const coordinates =
+JSON.parse(mapDiv.dataset.coordinates);
 
-    const coordinates = JSON.parse(
-        mapDiv.dataset.coordinates || "[]"
-    );
+const map = L.map("map").setView(
+    [coordinates[1],coordinates[0]],
+    13
+);
 
-    // stop if coordinates don't exist
-    if (coordinates.length === 2) {
-
-        const map = L.map("map").setView(
-            [coordinates[1], coordinates[0]],
-            13
-        );
-
-        L.tileLayer(
-            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            {
-                attribution: '© OpenStreetMap'
-            }
-        ).addTo(map);
-
-        L.marker(
-            [coordinates[1], coordinates[0]]
-        )
-        .addTo(map)
-        .bindPopup("Listing Location")
-        .openPopup();
-    }
+L.tileLayer(
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+ attribution:'© OpenStreetMap'
 }
+).addTo(map);
+
+L.marker(
+[coordinates[1],coordinates[0]]
+)
+.addTo(map)
+.bindPopup("Listing Location")
+.openPopup();
